22ee23ebe1c0903b0cd6acc0ed9c43f90d1349b1
stephenkuyon <stephenfractyldev@gmail.com>
2020-10-04 00:49:30 -0600

    About page

--- app/src/main/java/com/example/coveryourapps/AboutFragment.java
+++ app/src/main/java/com/example/coveryourapps/AboutFragment.java
@@ -13,8 +13,12 @@ import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment {
 
+
+
     @Nullable
     @Override
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
         
